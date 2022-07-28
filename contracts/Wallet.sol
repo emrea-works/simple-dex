5db1644da8b507c8aedf3e0e3e37045f77a19e3f
emrea-works <gelisim.odakli@gmail.com>
2022-07-28 14:54:03 -0300

    cleaned up wallet contract: fixed comments

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -3,18 +3,17 @@ pragma solidity >=0.6.0 <0.8.0;
 
 contract Wallet {
   // Storage Design
-  // struct
   struct Token {
     bytes32 ticker;
     address tokenAddress;
   }
-  // dynamic strcutreu betw array and mapping
+  // dynamic structure between array and mapping
   mapping(bytes32 => Token) public tokenMapping;
   bytes32[] public tokenList;
   // mapping: address given, amount of currency ticker symbols (bytes32) returns
   /*
-   * @dev currency symbols could be string type but when we need to compare
-   * values string is not capable to compare the values
+   * @dev: currency symbols could be string type but when we need to compare
+   * values string is not the type at compasion
    */
   mapping(address => mapping(bytes32 => uint256)) public balances;
   // function: add tokens, making external saves from gas consumption
